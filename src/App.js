19088a53771606b2dec0015c3a931ae5ad8b1437
Darya <darya.lapata@gmail.com>
2021-12-08 20:02:11 +0100

    set up project and tryed api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 export const App = () => {
+  fetch('https://dls-shows-api.herokuapp.com/shows')
+    .then((res) => res.json())
+    .then((json) => console.log(json.response))
+
   return (
     <div>
       Find me in src/app.js!
