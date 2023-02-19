32d4ee86f1d692f1bbbcdd4c81be916ea429ea81
DALA746 <darya.lapata@gmail.com>
2023-02-19 21:14:10 +0100

    changed card grid

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-# Project Express Api Frontend 
+# Project Express Api Frontend
 
 Frontend project for my backend repo: https://dls-shows-api.herokuapp.com/
-# View it live 
+
+# View it live
+
 https://dl-shows-api.netlify.app/
+
 # Want to know more about this project?
+
 Follow this link: https://github.com/DALA746/project-express-api
 
-
+![alt text](https://dl-shows-api.netlify.app/src/img/design.png)
--- src/components/ShowsList.js
+++ src/components/ShowsList.js
@@ -1,6 +1,9 @@
+/* eslint-disable react/jsx-closing-bracket-location */
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { TbMovie } from 'react-icons/tb';
+import { BsDisplay } from 'react-icons/bs';
 
 import { Jambotron } from './Jambotron';
 
@@ -8,29 +11,34 @@ const MainContent = styled.main`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
+  width: 100%;
 `;
 const CardContainer = styled.div`
-  display: flex;
-  margin: 0 20px;
-  flex-wrap: wrap;
-  justify-content: center;
-  justify-content: space-evenly;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 1rem;
+  padding: 60px 20px;
+  max-width: 1200px;
+  width: 100%;
 `;
 
 const Card = styled.div`
-  width: 50%;
-  height: auto;
+  background-color: var(--pink);
+  border-radius: 10px;
   padding: 20px;
-  border: 1px solid white;
-  margin: 10px auto;
+  height: 300px;
+  max-width: 400px;
 
-  :hover {
-    transition: 0.3s;
+  div {
+    display: flex;
+    justify-content: end;
   }
 
-  @media (min-width: 885px) {
-    width: 400px;
-    height: 300px;
+  :hover {
+    background-color: var(--orange);
+    color: var(--dark_blue);
+    transition: 0.6s ease-out;
   }
 `;
 
@@ -39,6 +47,11 @@ const ButtonContainer = styled.div`
   justify-content: center;
   margin: 20px auto;
   gap: 10px;
+`;
+
+const SearchContainer = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
 
 const Button = styled.button`
@@ -57,6 +70,7 @@ const Button = styled.button`
 
 export const ShowList = () => {
   const [shows, setShows] = useState([]);
+  const [title, setTitle] = useState('');
   const [pageNumber, setPageNumber] = useState(1);
   // const [pageLimit, setPageLimit] = useState(5)
   const pageLimit = 10;
@@ -66,9 +80,9 @@ export const ShowList = () => {
       `https://project-express-api-production.up.railway.app/shows?page=${pageNumber}&limit=${pageLimit}`
     )
       .then((res) => res.json())
-      .then((json) => {
-        setShows(json);
-        console.log(json);
+      .then((data) => {
+        console.log(data, 'DATA');
+        setShows(data);
       });
   }, [pageNumber, pageLimit]);
 
@@ -80,10 +94,64 @@ export const ShowList = () => {
     setPageNumber(pageNumber - 1);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch(
+      `https://project-express-api-production.up.railway.app/shows?title=${title}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data, 'DATA FROM SEARCH');
+        setShows(data);
+        setTitle('');
+      })
+      .catch(() => {
+        console.error();
+        setTitle('');
+      });
+  };
+
+  console.log(shows, 'this is shows');
+
   return (
     <>
       <Jambotron />
+
       <MainContent>
+        <h1>Our Movies & TV Series</h1>
+        <SearchContainer>
+          <input
+            type="text"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+          <Button type="button" onClick={handleSubmit}>
+            Search by title
+          </Button>
+        </SearchContainer>
+
+        <CardContainer>
+          {shows.success ? (
+            shows.response.map((show) => (
+              <Link key={show.show_id} to={`/shows/${show.show_id}`}>
+                <Card key={show.show_id}>
+                  <div>
+                    {show.type === 'Movie' ? (
+                      <TbMovie className="icons" />
+                    ) : (
+                      <BsDisplay className="icons" />
+                    )}
+                  </div>
+                  <h2>{show.title}</h2>
+                  {show.description === '' ? <p /> : <p>{show.description}</p>}
+                </Card>
+              </Link>
+            ))
+          ) : (
+            <p>No results</p>
+          )}
+        </CardContainer>
         <ButtonContainer>
           <p>
             {' '}
@@ -99,17 +167,6 @@ export const ShowList = () => {
             Next Page
           </Button>
         </ButtonContainer>
-        <CardContainer>
-          {shows.map((show) => (
-            <Link key={show.show_id} to={`/shows/${show.show_id}`}>
-              <Card key={show.show_id}>
-                <h1>{show.title}</h1>
-                {show.description === '' ? <p /> : <p>{show.description}</p>}
-                <Button type="button">More details</Button>
-              </Card>
-            </Link>
-          ))}
-        </CardContainer>
       </MainContent>
     </>
   );
