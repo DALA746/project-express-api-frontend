bc7b3862dd03bf01c381e2dedea0f4ab9064965e
DALA746 <darya.lapata@gmail.com>
2023-03-22 16:57:41 +0100

    added error displaying in fetch function

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,10 +1,15 @@
 const BASE_URL = 'https://project-express-api-production.up.railway.app';
 
 export const fetchData = async (endpoint) => {
-  const response = await fetch(`${BASE_URL}${endpoint}`);
-  const data = await response.json();
-  if (!data) {
-    alert('Something is wrong!');
+  try {
+    const response = await fetch(`${BASE_URL}${endpoint}`);
+    if (!response.ok) {
+      throw new Error(`${response.status} ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    alert('Something is wrong!', error);
   }
-  return data;
 };
